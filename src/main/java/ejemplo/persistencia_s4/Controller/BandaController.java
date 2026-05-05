7b50e8af6c8e902a2f04a923c59577253f963456
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-05-05 14:24:35 -0400

    Todo listo

---
+++ src/main/java/ejemplo/persistencia_s4/Controller/BandaController.java
@@ -0,0 +1,10 @@
+package ejemplo.persistencia_s4.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/bandas")
+public class BandaController {
+    
+}
